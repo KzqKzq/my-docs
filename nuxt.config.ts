062633713ab3e68ea8a3b1a9c9efff89b172a48a
kangziqiang <1044075786@qq.com>
2025-12-22 10:55:32 +0800

    feat: initialize project with Nuxt and Docus dependencies
    - Add package.json with essential scripts and dependencies for Nuxt and Docus.
    - Include favicon.ico and mountains.webp assets in the public directory.
    - Create tsconfig.json for TypeScript configuration with references to Nuxt generated tsconfig files.

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxtjs/i18n', 'nuxt-studio'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [{
+      code: 'en',
+      name: 'English',
+    }, {
+      code: 'fr',
+      name: 'Français',
+    }],
+  },
+    studio: {
+    // Studio admin route (default: '/_studio')
+    route: '/_studio',
+    
+    // Git repository configuration (owner and repo are required)
+    repository: {
+      provider: 'github', // 'github' or 'gitlab'
+      owner: 'KzqKzq', // your GitHub/GitLab username or organization
+      repo: 'my-docs', // your repository name
+      branch: 'main', // the branch to commit to (default: main)
+    }
+  }
+})
